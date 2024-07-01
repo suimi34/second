67701f9df5bdd619b4c9d48b4c23d3bde651e852
Susumu Ishikawa <susumu2934@gmail.com>
2024-07-02 01:28:18 +0900

    keyframes

--- src/components/ChildForm.vue
+++ src/components/ChildForm.vue
@@ -11,6 +11,11 @@ export default {
       default: "",
     }
   },
+  data() {
+    return {
+      isActive: true,
+    };
+  },
   created() {
     console.log(this.value);
     console.log("parent term", this.parentTerm);
@@ -18,7 +23,17 @@ export default {
   watch: {
     parentTerm: function (val) {
       console.log("[watch ]parent term", val);
+      if (val.length > 4) {
+        this.isActive = false;
+      } else {
+        this.isActive = true;
+      }
     }
+  },
+  computed: {
+    searchText() {
+      return this.isActive ? "Active" : "Inactive";
+    },
   },
   methods: {
     updateName(name) {
@@ -41,9 +56,42 @@ export default {
 </script>
 
 <template>
-
   <div>
-    <input type="text" :value="value.name" @input="updateName($event.target.value)" />
-    <input type="number" :value="value.age"  @input="updateAge($event.target.value)" />
+    <div>
+      <input type="text" :value="value.name" @input="updateName($event.target.value)" />
+      <input type="number" :value="value.age"  @input="updateAge($event.target.value)" />
+    </div>
+    <button :class="{ appear: isActive, disa: !isActive }">{{  searchText }}</button>
   </div>
 </template>
+
+<style scoped>
+
+.disa {
+  opacity: .3;
+  animation: disa 1s;
+}
+
+@keyframes disa {
+  from {
+    opacity: .7;
+  }
+  to {
+    opacity: .3;
+  }
+}
+
+.appear {
+  opacity: 1;
+  animation: appear 1s;
+}
+
+@keyframes appear{
+  from {
+    opacity: .5;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
